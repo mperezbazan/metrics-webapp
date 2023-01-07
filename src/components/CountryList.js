33995d99b2dd6fada34ee28bfac82bc02670d618
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-07 10:02:13 -0300

    Add navbar and loader

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -1,33 +1,51 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { getWeatherAPI } from '../redux/Country/Country';
 import Country from './Country';
+import Loading from './Loading';
+import Navbar from './Navbar';
 
 const CountryList = () => {
+  const [mainCountry, setMainCountry] = useState();
+  const [filteredCountries, setFilteredCountries] = useState([]);
   const countries = useSelector((state) => state.countries);
   useEffect(() => {
+    setMainCountry(Math.floor(Math.random() * 243));
     getWeatherAPI();
+    setFilteredCountries(countries);
   }, []);
+  const handleChange = (e) => {
+    const search = e.target.value;
+    const newCountries = [...countries];
+    const data = newCountries.filter((country) => (
+      country.country.toLowerCase().includes(search.toLowerCase())));
+    setFilteredCountries(data);
+  };
   return (
     <>
-      {
-        countries.length > 0
-        && (
-
-        <div>
-          <div>NAVBAR</div>
-          <Country item={countries[Math.floor(Math.random() * countries.length)]} fullwidth />
-          <p className="home-title">WEATHER BY COUNTRY</p>
-          <div className="countries-container">
-            {
-              countries.map((country) => (
-                <Country key={country.alpha3} item={country} />
-              ))
-            }
-          </div>
-        </div>
-        )
+      <div>
+        <Navbar />
+        {
+        countries.length === 0
+          ? <Loading />
+          : (
+            <>
+              <Country item={countries[mainCountry]} fullwidth />
+              <div className="home-mid-container">
+                <p className="home-title">WEATHER BY COUNTRY</p>
+                <input type="text" onChange={handleChange} className="search-input" placeholder="Search by country" />
+              </div>
+              <div className="countries-container">
+                {
+                  filteredCountries.map((country) => (
+                    <Country key={country.alpha3} item={country} />
+                  ))
+                }
+              </div>
+            </>
+          )
       }
+      </div>
     </>
   );
 };
