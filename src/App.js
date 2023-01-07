e311622d77263cc19b0914b07b10a5c7cbdc57d5
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-07 08:20:26 -0300

    Add routes to Home and get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { getWeatherAPI } from './redux/Country/Country';
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getWeatherAPI());
+  }, [dispatch]);
   return (
-    <div className="App">
-      Test
+    <div className="main-container">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
